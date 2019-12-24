c824f15e85ef85c3fcc30855f0573a6ab4d13212
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2019-12-24 10:07:43 +0900

    Add support for Buster, Xenial, Bionic

--- setup.sh
+++ setup.sh
@@ -8,3 +8,8 @@ set -x
 mkdir -p $BUILD_DIR
 cp -frp $SCRIPT_DIR/debian $BUILD_DIR
 cp -frp $SCRIPT_DIR/files/* $BUILD_DIR
+
+cd $BUILD_DIR
+sudo apt-get update
+sudo apt-get -y upgrade
+dpkg-checkbuilddeps 2>&1 | sed 's/dpkg-checkbuilddeps.*dependencies: //' | xargs sudo apt-get -y install
