8c4861b80780c6c78cb4fb7f6d2487f2821168f5
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2022-08-04 13:08:10 +0900

    add support for bullseye and jammy

--- setup.sh
+++ setup.sh
@@ -1,15 +1,1 @@
-#!/bin/sh
-. $(dirname $0)/path.sh
-test -z $BUILD_DIR && exit 127
-
-rm -rf $BUILD_DIR
-set -x
-
-mkdir -p $BUILD_DIR
-cp -frp $SCRIPT_DIR/debian $BUILD_DIR
-cp -frp $SCRIPT_DIR/files/* $BUILD_DIR
-
-cd $BUILD_DIR
-sudo apt-get update
-sudo apt-get -y upgrade
-dpkg-checkbuilddeps 2>&1 | sed 's/dpkg-checkbuilddeps.*dependencies: //' | xargs sudo apt-get -y install
+../ma-common/script/setup-nosrc.sh
